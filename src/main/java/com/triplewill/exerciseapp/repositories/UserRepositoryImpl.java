9d2dcc0600ef93fc516be7a27e379b5a7d0d0c56
odjur37 <erikw.englund@gmail.com>
2020-01-03 02:19:36 +0100

    refactor to use jpa

--- src/main/java/com/triplewill/exerciseapp/repositories/UserRepositoryImpl.java
+++ src/main/java/com/triplewill/exerciseapp/repositories/UserRepositoryImpl.java
@@ -1,44 +1,11 @@
 package com.triplewill.exerciseapp.repositories;
 
-import java.util.List;
-
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.triplewill.exerciseapp.mapper.UserRowMapper;
 import com.triplewill.model.User;
 
 @Repository
-public class UserRepositoryImpl implements UserRepository{
-
-	private NamedParameterJdbcTemplate template;
-	
-	public UserRepositoryImpl(NamedParameterJdbcTemplate template) {
-		this.template = template;
-	}
-	
-	@Override
-	public List<User> findById(String id) {
-		return template.query("SELECT * FROM users WHERE userId='"+id+"'", new UserRowMapper());
-	}
-	
-	@Override
-	public List<User> findAll() {
-		return template.query("SELECT * FROM users", new UserRowMapper());
-	}
-
-	@Override
-	public void updateUser(User user) {
-		final String sql = "update users set exercises=:exercises where userId=':userId'";
-	        KeyHolder holder = new GeneratedKeyHolder();
-	        SqlParameterSource param = new MapSqlParameterSource()
-	        		.addValue("userId", user.getId())
-	        		.addValue("exercises", user.getExercises());
-	        template.update(sql,param, holder);
-	}
+public interface UserRepositoryImpl extends JpaRepository<User, String>{
 
 }
--- src/main/java/com/triplewill/exerciseapp/services/UserService.java
+++ src/main/java/com/triplewill/exerciseapp/services/UserService.java
@@ -1,7 +1,7 @@
 package com.triplewill.exerciseapp.services;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,22 +16,17 @@ public class UserService {
 	UserRepositoryImpl repo;
 	
 	public List<User> getAllUsers(){
-		List<User> result = new ArrayList<User>();
-		Iterable<User> iter = this.repo.findAll();
-		iter.forEach(result::add);
-		return result;
+		return this.repo.findAll();
 	}
 
 	public void updateUser(String body) {
 		System.err.println("Before execution: " + body);
-		List<User> queryResult = this.repo.findById(body);
-		if (!queryResult.isEmpty()) {
-			System.err.println("inside if case ----");
-			User p = queryResult.get(0);
-			System.err.println("Exercises for user: " + p.getName() + " is: " + p.getExercises());
+		Optional<User> queryResult=this.repo.findById(body);
+		if (queryResult.isPresent()) {
+			System.err.println("AT LAST INSIDE!!!");
+			User p = queryResult.get();
 			p.setExercises(p.getExercises()+1);
-			System.err.println("Exercises is now: " + p.getExercises());
-			this.repo.updateUser(p);
+			this.repo.save(p);
 		}
 	}
 }
