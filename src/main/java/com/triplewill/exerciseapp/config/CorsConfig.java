973464d5f319377b0a48c8da18ae2a85302cc73b
Erik Englund <erik.englund@callistaenterprise.se>
2020-03-16 23:10:43 +0100

    fix cors to allow multiple hosts

--- src/main/java/com/triplewill/exerciseapp/config/CorsConfig.java
+++ src/main/java/com/triplewill/exerciseapp/config/CorsConfig.java
@@ -10,12 +10,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer{
 	
-	@Value("${allow.origin.host}")
-	public String hostOrigin;
+	@Value("${web.cors.allowed-origins}")
+	public String[] corsAllowedOrigins;
 	
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins(hostOrigin).allowedMethods("*");
+		registry.addMapping("/**").allowedOrigins(corsAllowedOrigins).allowedMethods("*");
 	}
 	
 }
